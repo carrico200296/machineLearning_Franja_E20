05b751f7e1bd9d5cd4a5154c42523bc992deb512
Vice Roncevic <vice.roncevic@gmail.com>
2020-10-31 22:38:41 +0100

    Added another remote point

--- scripts/regression.py
+++ scripts/regression.py
@@ -3,6 +3,7 @@ Regression analysis
 Created: 30.10.2020
 """
 import sklearn.linear_model as lm
+from sklearn import model_selection
 from matplotlib.pyplot import figure, plot, xlabel, ylabel, legend, show
 
 # Import configuration file that determines the dataset to be used
@@ -14,11 +15,17 @@ from concNoZero_config import *
 xIn = X_stand
 yIn = y_stand 
 
+# Simple holdout-set crossvalidation
+test_proportion = 0.5
+X_train, X_test, y_train, y_test = model_selection.train_test_split(xIn,yIn,test_size=test_proportion)
+
 model = lm.LinearRegression(fit_intercept=True)
-model = model.fit(xIn,yIn)
+model = model.fit(X_train,y_train)
 
-y_est = model.predict(xIn)
+y_est_train = np.asarray(model.predict(X_train), dtype=float) 
+y_est_test = np.asarray(model.predict(X_test),dtype=float)
 
+"""
 # Plot original data and the model output
 f = figure()
 
@@ -27,6 +34,15 @@ xlabel('yIn'); ylabel('y predicted')
 legend(['Training data', 'Regression fit (model)'])
 
 show()
+"""
+
+
+
+
+
+
+
+# Add vector of ones before the regularization
 
 
 
